957adc2a13c69fc2a551553c305731ead4a53995
Stephan Seifermann <seiferma@users.noreply.github.com>
2021-02-27 16:24:34 +0100

    Use new base image.

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,16 +8,5 @@ if [ $? -ne 0 ]; then
     exit 1
 fi
 
-# Apply nft definitions
-nft -f /etc/nftables.conf
-if [ $? -ne 0 ]; then
-    echo "ERROR: Could not apply nftables definitions."
-    echo "Try running the container with the capabilities NET_ADMIN and NET_RAW, e.g. by adding \"--cap-add=NET_ADMIN --cap-add=NET_RAW\" to your command."
-    exit 1
-fi
-
-# Run tor under user "tor"
-su tor -s /bin/sh -c "tor -f /etc/tor/torrc" > /tmp/tor.log &
-
-# Execute command
-exec "$@"
+# Pass everything to the original entrypoint
+exec /entrypoint.sh "$@"
